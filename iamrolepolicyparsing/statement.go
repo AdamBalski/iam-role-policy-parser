c9b9db662b1e1fe90f9d8d71c627b525ed419b49
AdamBalski <adambalski1@gmail.com>
2024-04-22 12:22:24 +0200

    ADD: iamrolepolicy statement parsing and validation

---
+++ iamrolepolicyparsing/statement.go
@@ -0,0 +1,193 @@
+package iamrolepolicyparsing
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+type Statement struct {
+	Sid            *string `json:"Sid"`
+	PrincipalValue interface{}
+	Principal      bool
+	ActionValue    interface{}
+	Action         bool
+	ResourceValue  interface{}
+	Resource       bool
+	Effect         *string     `json:"Effect"`
+	ConditionMap   interface{} `json:"Condition"`
+}
+
+func stringPtrToString(ptr *string) string {
+	if ptr == nil {
+		return "nil"
+	}
+	return *ptr
+}
+
+func (this *Statement) String() string {
+	return fmt.Sprintf(
+		"Statement{Sid: %v, PrincipalValue: %v, Principal: %v, ActionValue: %v, Action: %v, ResourceValue: %v, Resource: %v, Effect: %v, ConditionMap: %v}",
+		stringPtrToString(this.Sid),
+		this.PrincipalValue,
+		this.Principal,
+		this.ActionValue,
+		this.Action,
+		this.ResourceValue,
+		this.Resource,
+		stringPtrToString(this.Effect),
+		this.ConditionMap,
+	)
+}
+
+func (this *Statement) Equals(other interface{}) bool {
+	if that, ok := other.(Statement); ok {
+		return !(!reflect.DeepEqual(this.ActionValue, that.ActionValue) ||
+			!reflect.DeepEqual(this.ResourceValue, that.ResourceValue) ||
+			!reflect.DeepEqual(this.ConditionMap, that.ConditionMap) ||
+			this.Sid != nil && *this.Sid != *that.Sid ||
+			this.Effect != nil && *this.Effect != *that.Effect ||
+			this.ResourceValue != nil && this.Resource != that.Resource ||
+			this.ActionValue != nil && this.Action != that.Action ||
+			this.PrincipalValue != nil && this.Principal != that.Principal)
+	}
+	return false
+
+}
+
+func (stat *Statement) UnmarshalJSON(data []byte) error {
+	var statMap map[string]interface{}
+	err := json.Unmarshal(data, &statMap)
+	if err != nil {
+		return err
+	}
+
+	// Ensure no unwanted properties exist in data
+	for key, _ := range statMap {
+		if key != "Sid" &&
+			key != "Principal" &&
+			key != "Action" &&
+			key != "Resource" &&
+			key != "NotAction" &&
+			key != "Effect" &&
+			key != "Condition" &&
+			key != "NotResource" &&
+			key != "NotPrincipal" {
+			return errors.New(fmt.Sprintf("unknown key: %s", key))
+		}
+	}
+
+	// Sid block parsing
+	if statMap["Sid"] != nil {
+		if sid, ok := statMap["Sid"].(string); !ok {
+			return errors.New("sid is a non-string")
+		} else {
+			stat.Sid = &sid
+		}
+	}
+
+	// Effect block
+	if effect, ok := statMap["Effect"].(string); !ok {
+		return errors.New("effect is absent or a non-string")
+	} else {
+		if effect != "Allow" && effect != "Deny" {
+			return errors.New(`effect should be either "Allow" or "Deny"`)
+		}
+		stat.Effect = &effect
+	}
+
+	// Principal block
+	if statMap["Principal"] != nil {
+		stat.PrincipalValue = statMap["Principal"]
+		stat.Principal = true
+	}
+	if statMap["NotPrincipal"] != nil {
+		if stat.PrincipalValue != nil {
+			return errors.New("principal and not-principal value shouldn't exist within a single statement block")
+		}
+		stat.PrincipalValue = statMap["NotPrincipal"]
+		stat.Principal = false
+	}
+	if principalString, ok := stat.PrincipalValue.(string); ok {
+		if principalString != "*" {
+			return errors.New("principal value should be '*' or a map")
+		}
+	} else if principalMap, ok := stat.PrincipalValue.(map[string]interface{}); ok {
+		for key, value := range principalMap {
+			if key != "AWS" && key != "Federated" && key != "Service" && key != "CanonicalUser" {
+				return errors.New(`key in principal map should be one of the following: "AWS", "Federated", "Service", "CanonicalUser"`)
+			}
+			if array, ok := value.([]interface{}); ok {
+				for _, principalIdString := range array {
+					if _, ok := principalIdString.(string); !ok {
+						return errors.New("value in principal map should be a []string")
+					}
+				}
+			} else {
+				return errors.New("value in principal map should be an array")
+			}
+		}
+	} else if stat.PrincipalValue != nil {
+		return errors.New("principal value should be '*' or a map")
+	}
+
+	// Action Block
+	if statMap["Action"] != nil {
+		stat.ActionValue = statMap["Action"]
+		stat.Action = true
+	}
+	if statMap["NotAction"] != nil {
+		if stat.ActionValue != nil {
+			return errors.New("action and not-action shouldn't exist within a single statement block")
+		}
+		stat.ActionValue = statMap["NotAction"]
+		stat.Action = false
+	}
+	if stat.ActionValue == nil {
+		return errors.New("action or not-action has to exist in a statement block")
+	}
+	switch stat.ActionValue.(type) {
+	case string:
+	case []interface{}:
+		for _, action := range stat.ActionValue.([]interface{}) {
+			if _, ok := action.(string); !ok {
+				return errors.New("action value should be a []string")
+			}
+		}
+	default:
+		return errors.New("action value should either be a string or a []string")
+	}
+
+	// Resource block
+	if statMap["Resource"] != nil {
+		stat.ResourceValue = statMap["Resource"]
+		stat.Resource = true
+	}
+	if statMap["NotResource"] != nil {
+		if stat.ResourceValue != nil {
+			return errors.New("resource and not-resource shouldn't exist within a single statement block")
+		}
+		stat.ResourceValue = statMap["NotResource"]
+		stat.Resource = false
+	}
+	if stat.ResourceValue == nil {
+		return errors.New("resource or not-resource has to exist in a statement block")
+	}
+	switch stat.ResourceValue.(type) {
+	case string:
+	case []interface{}:
+		for _, resource := range stat.ResourceValue.([]interface{}) {
+			if _, ok := resource.(string); !ok {
+				return errors.New("resource value should be a []string")
+			}
+		}
+	default:
+		return errors.New("resource value should either be a string or a []string")
+	}
+
+	// Condition block
+	stat.ConditionMap = statMap["Condition"]
+
+	return nil
+}
---
+++ iamrolepolicyparsing/statement_test.go
@@ -0,0 +1,400 @@
+package iamrolepolicyparsing
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func stringOf(s string) *string {
+	return &s
+}
+
+func TestStatement_String(t *testing.T) {
+	stat := Statement{
+		Sid:            stringOf("123"),
+		Effect:         stringOf("Allow"),
+		Principal:      true,
+		Action:         true,
+		Resource:       true,
+		PrincipalValue: map[string]interface{}{"AWS": []string{"arn:aws:iam::123456789012:user/JohnDoe"}},
+		ActionValue:    []interface{}{"s3:ListBucket"},
+		ResourceValue:  []interface{}{"arn:aws:s3:::example-bucket"},
+	}
+	expected := "Statement{Sid: 123, PrincipalValue: map[AWS:[arn:aws:iam::123456789012:user/JohnDoe]], Principal: true, ActionValue: [s3:ListBucket], Action: true, ResourceValue: [arn:aws:s3:::example-bucket], Resource: true, Effect: Allow, ConditionMap: <nil>}"
+	if stat.String() != expected {
+		t.Errorf("Expected: \n%v\n\t, got: \n%v", expected, stat.String())
+	}
+}
+
+func TestStatement_EqualsIfNotEqual(t *testing.T) {
+	stat1 := Statement{
+		Sid:            stringOf("123"),
+		Effect:         stringOf("Allow"),
+		Principal:      true,
+		Action:         true,
+		Resource:       true,
+		PrincipalValue: map[string]interface{}{"AWS": []string{"arn:aws:iam::123456789012:user/JohnDoe"}},
+		ActionValue:    []interface{}{"s3:ListBucket"},
+		ResourceValue:  []interface{}{"arn:aws:s3:::example-bucket"},
+	}
+	stat2 := Statement{
+		Sid:            stringOf("123"),
+		Effect:         stringOf("Allow"),
+		Principal:      false,
+		Action:         true,
+		Resource:       true,
+		PrincipalValue: map[string]interface{}{"AWS": []string{"arn:aws:iam::123456789012:user/JohnDoe"}},
+		ActionValue:    []interface{}{"s3:ListBucket"},
+		ResourceValue:  []interface{}{"arn:aws:s3:::example-bucket"},
+	}
+	if stat1.Equals(stat2) {
+		t.Errorf("Expected: not equal, got: equal")
+	}
+}
+
+func TestStatement_EqualsIfEqual(t *testing.T) {
+	stat1 := Statement{
+		Sid:            stringOf("123"),
+		Effect:         stringOf("Allow"),
+		Principal:      true,
+		Action:         true,
+		Resource:       true,
+		PrincipalValue: map[string]interface{}{"AWS": []string{"arn:aws:iam::123456789012:user/JohnDoe"}},
+		ActionValue:    []interface{}{"s3:ListBucket"},
+		ResourceValue:  []interface{}{"arn:aws:s3:::example-bucket"},
+	}
+	stat2 := Statement{
+		Sid:            stringOf("123"),
+		Effect:         stringOf("Allow"),
+		Principal:      true,
+		Action:         true,
+		Resource:       true,
+		PrincipalValue: map[string]interface{}{"AWS": []string{"arn:aws:iam::123456789012:user/JohnDoe"}},
+		ActionValue:    []interface{}{"s3:ListBucket"},
+		ResourceValue:  []interface{}{"arn:aws:s3:::example-bucket"},
+	}
+	if !stat1.Equals(stat2) {
+		t.Errorf("Expected: equal, got: not equal")
+	}
+}
+
+func TestStatement_EqualsIfNil(t *testing.T) {
+	stat1 := Statement{
+		Sid:            stringOf("123"),
+		Effect:         stringOf("Allow"),
+		Principal:      true,
+		Action:         true,
+		Resource:       true,
+		PrincipalValue: map[string]interface{}{"AWS": []string{"arn:aws:iam::123456789012:user/JohnDoe"}},
+		ActionValue:    []interface{}{"s3:ListBucket"},
+		ResourceValue:  []interface{}{"arn:aws:s3:::example-bucket"},
+	}
+	if stat1.Equals(nil) {
+		t.Errorf("Expected: not equal, got: equal")
+	}
+}
+
+func TestStatement_EqualsIfNotAStatement(t *testing.T) {
+	stat1 := Statement{
+		Sid:            stringOf("123"),
+		Effect:         stringOf("Allow"),
+		Principal:      true,
+		Action:         true,
+		Resource:       true,
+		PrincipalValue: map[string]interface{}{"AWS": []string{"arn:aws:iam::123456789012:user/JohnDoe"}},
+		ActionValue:    []interface{}{"s3:ListBucket"},
+		ResourceValue:  []interface{}{"arn:aws:s3:::example-bucket"},
+	}
+	var stat2 interface{} = "Not a Statement"
+	if stat1.Equals(stat2) {
+		t.Errorf("Expected: false, got: true")
+	}
+}
+
+func TestStatement_UnmarshalValid1(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"Action":["s3:ListBucket"],"NotResource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	var expectedErr = (error)(nil)
+	err := stat.UnmarshalJSON(data)
+
+	expected := Statement{
+		Sid:            stringOf("123"),
+		Effect:         stringOf("Allow"),
+		Principal:      true,
+		Action:         true,
+		Resource:       false,
+		PrincipalValue: map[string]interface{}{"AWS": []string{"arn:aws:iam::123456789012:user/JohnDoe"}},
+		ActionValue:    []interface{}{"s3:ListBucket"},
+		ResourceValue:  "arn:aws:s3:::example-bucket",
+	}
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+	if !stat.Equals(expected) {
+		t.Errorf("Expected: \n%v\n\t, got: \n%v", expected.String(), stat.String())
+	}
+}
+
+func TestStatement_UnmarshalValid2(t *testing.T) {
+	data := []byte(`{"Effect":"Allow","NotPrincipal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"NotAction":"s3:ListBucket","Resource":["arn:aws:s3:::example-bucket"]}`)
+	var stat Statement
+	var expectedErr = (error)(nil)
+	err := stat.UnmarshalJSON(data)
+
+	expected := Statement{
+		Effect:         stringOf("Allow"),
+		Principal:      false,
+		Action:         false,
+		Resource:       true,
+		PrincipalValue: map[string]interface{}{"AWS": []string{"arn:aws:iam::123456789012:user/JohnDoe"}},
+		ActionValue:    "s3:ListBucket",
+		ResourceValue:  []interface{}{"arn:aws:s3:::example-bucket"},
+	}
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+	if !stat.Equals(expected) {
+		t.Errorf("Expected: \n%v\n\t, got: \n%v", expected.String(), stat.String())
+	}
+}
+
+func TestStatement_UnmarshalValid3(t *testing.T) {
+	data := []byte(`{"Effect":"Allow","NotAction":"s3:ListBucket","Resource":["arn:aws:s3:::example-bucket"]}`)
+	var stat Statement
+	var expectedErr = (error)(nil)
+	err := stat.UnmarshalJSON(data)
+
+	expected := Statement{
+		Sid:            nil,
+		Effect:         stringOf("Allow"),
+		Principal:      true,
+		Action:         false,
+		Resource:       true,
+		PrincipalValue: nil,
+		ActionValue:    "s3:ListBucket",
+		ResourceValue:  []interface{}{"arn:aws:s3:::example-bucket"},
+	}
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+	if !stat.Equals(expected) {
+		t.Errorf("Expected: \n%v\n\t, got: \n%v", expected.String(), stat.String())
+	}
+}
+
+func TestStatement_UnmarshalInvalidJSON(t *testing.T) {
+	data := []byte(`invalid json`)
+	var stat Statement
+	expectedErr := errors.New("invalid character 'i' looking for beginning of value")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err.Error(), expectedErr.Error()) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalUnwantedProperty(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"Action":"s3:ListBucket","Resource":"arn:aws:s3:::example-bucket","Unwanted":"property"}`)
+	var stat Statement
+	expectedErr := errors.New("unknown key: Unwanted")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalMissingEffect(t *testing.T) {
+	data := []byte(`{"Sid":"123","Principal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"Action":"s3:ListBucket","Resource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New("effect is absent or a non-string")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalNonStringEffect(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":123,"Principal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"Action":"s3:ListBucket","Resource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New("effect is absent or a non-string")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalInvalidEffect(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Invalid","Principal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"Action":"s3:ListBucket","Resource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New(`effect should be either "Allow" or "Deny"`)
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalInvalidSidType(t *testing.T) {
+	data := []byte(`{"Sid":123,"Effect":"Allow","Principal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"Action":"s3:ListBucket","Resource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New("sid is a non-string")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalInvalidActionType(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"Action":123,"Resource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New("action value should either be a string or a []string")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalPrincipalAndNotPrincipalExist(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"NotPrincipal":{"AWS":["arn:aws:iam::123456789012:user/JaneDoe"]},"Action":"s3:ListBucket","Resource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New("principal and not-principal value shouldn't exist within a single statement block")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalActionAndNotActionExist(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"Action":["s3:ListBucket"],"NotAction":["s3:GetObject"],"Resource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New("action and not-action shouldn't exist within a single statement block")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalResourceAndNotResourceExist(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"Action":"s3:ListBucket","Resource":"arn:aws:s3:::example-bucket","NotResource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New("resource and not-resource shouldn't exist within a single statement block")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalInvalidResourceType(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"Action":"s3:ListBucket","Resource":123}`)
+	var stat Statement
+	expectedErr := errors.New("resource value should either be a string or a []string")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalPrincipalValueIsNonAsteriskString(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":"lol","Action":"s3:ListBucket","Resource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New("principal value should be '*' or a map")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalPrincipalValueIsOfInvalidType(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":123,"Action":"s3:ListBucket","Resource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New("principal value should be '*' or a map")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalPrincipalMapValueIsNotAStringSlice(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":{"AWS":[1,"arn:aws:iam::123456789012:user/JohnDoe"]},"Action":"s3:ListBucket","Resource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New("value in principal map should be a []string")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalPrincipalMapValueIsNotASlice(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":{"AWS":"arn:aws:iam::123456789012:user/JohnDoe"},"Action":"s3:ListBucket","Resource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New("value in principal map should be an array")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalPrincipalMapKeyIsNotValid(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":{"Invalid":["arn:aws:iam::123456789012:user/JohnDoe"]},"Action":"s3:ListBucket","Resource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New(`key in principal map should be one of the following: "AWS", "Federated", "Service", "CanonicalUser"`)
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalActionOrAndNotActionNotExisting(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"Resource":"arn:aws:s3:::example-bucket"}`)
+	var stat Statement
+	expectedErr := errors.New("action or not-action has to exist in a statement block")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
+
+func TestStatement_UnmarshalMissingResourceOrNotResource(t *testing.T) {
+	data := []byte(`{"Sid":"123","Effect":"Allow","Principal":{"AWS":["arn:aws:iam::123456789012:user/JohnDoe"]},"Action":"s3:ListBucket"}`)
+	var stat Statement
+	expectedErr := errors.New("resource or not-resource has to exist in a statement block")
+
+	err := stat.UnmarshalJSON(data)
+
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+}
